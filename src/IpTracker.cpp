aa14ff1e55505583f5bc5f872effea96967026db
FrancescoBorzi <borzifrancesco@gmail.com>
2021-03-20 15:59:13 +0100

    fix: use async queries

--- src/IpTracker.cpp
+++ src/IpTracker.cpp
@@ -5,7 +5,6 @@
 
 #define CONF_ENABLE_TRACKER "IpTracker.Enabled"
 #define CONF_CLEAN_OLDER_THAN_DAYS "IpTracker.CleanOlderThanDays"
-#define QUERY_SELECT_IP "SELECT `ip` FROM `account_ip` WHERE `account` = %u AND `ip` = '%s'"
 #define QUERY_INSERT_IP "INSERT IGNORE INTO `account_ip` (`account`, `ip`, `first_time`, `last_time`) VALUES (%u, '%s', NOW(), NOW());"
 #define QUERY_UPDATE_IP "UPDATE `account_ip` SET `last_time` = NOW() WHERE `account` = %u AND `ip` = '%s'"
 #define QUERY_DELETE_IP "DELETE FROM `account_ip` WHERE `last_time` < (DATE_SUB(NOW(), INTERVAL %u DAY))"
@@ -22,16 +21,8 @@ public:
             return;
         }
 
-        auto result = LoginDatabase.PQuery(QUERY_SELECT_IP, accountId, ip.c_str());
-
-        if (!result || result->GetRowCount() == 0)
-        {
-            LoginDatabase.PQuery(QUERY_INSERT_IP, accountId, ip.c_str());
-        }
-        else
-        {
-            LoginDatabase.PQuery(QUERY_UPDATE_IP, accountId, ip.c_str());
-        }
+        LoginDatabase.AsyncPQuery(QUERY_INSERT_IP, accountId, ip.c_str());
+        LoginDatabase.AsyncPQuery(QUERY_UPDATE_IP, accountId, ip.c_str());
     }
 
     void OnStartup() override
